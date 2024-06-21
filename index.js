45edd4ad883da0248b628012b28c5c10461425ca
BryamSenac <bryam.assolini@docente.pr.senac.br>
2024-06-21 17:15:40 -0300

    att 1 funcionalidade calculadora

---
+++ index.js
@@ -0,0 +1,44 @@
+let valorTemp = '0';
+let resultado;
+let operacao = '+';
+let valorUm = '0';
+let valorDois = '0';
+
+function attValor(valor) {
+    if (valorTemp === '0') {
+        valorTemp = valor;
+    } else {
+        valorTemp = valorTemp + valor;
+    }
+    console.log(valorTemp);
+}
+
+function escolheOperacao(op) {
+    operacao = op;
+    valorUm = valorTemp;
+    valorTemp = '0';
+    console.log(operacao);
+}
+
+function calcular() {
+    tagResultado = document.getElementById("resultado");
+    valorDois = valorTemp;
+    valorTemp = '0';
+
+    if (operacao === '+') {
+        resultado = Number(valorUm) + Number(valorDois);
+    } else if (operacao === '-') {
+        resultado = Number(valorUm) - Number(valorDois);
+    } else if (operacao === '*') {
+        resultado = Number(valorUm) * Number(valorDois);
+    } else if (operacao === '/') {
+        if (valorDois === '0') {
+            resultado = 'Valor dois invalido';
+        } else {
+            resultado = Number(valorUm) / Number(valorDois);
+        }
+    }
+
+    tagResultado.innerHTML = resultado;
+    console.log(resultado);
+}
