9cd653818f8854013edac8c0a2b07b6cf3fead68
BryamFLA <bryam.assolini@docente.pr.senac.br>
2024-07-05 21:57:00 -0300

    att final
    Funcionalidades finais da calculadora

--- js/calculadora.js
+++ js/calculadora.js
@@ -2,16 +2,108 @@ export class Calculadora{
     constructor(){
         this.resultado = document.getElementById('resultado');
         this.previa = document.getElementById('previa');
-        this.v1 = 0;
-        this.v2 = 0;
+        this.historico = document.getElementById('historico');
+
+        this.valorPrincipal = '0';
+        this.valorSecundario = '0';
+        this.resultadoCalculo = '0';
+        this.operacao = '+';
+        this.calculando = false;
+        this.historicoCalculos = [];
     }
 
     attValores(){
-        this.resultado.textContent = v1;
-        this.previa.textContent = v2;
+        this.resultado.textContent = this.valorPrincipal;
+        this.previa.textContent = this.valorSecundario;
+
+        this.historico.innerHTML = '';
+        for ( let i = 0; i < this.historicoCalculos.length; i++){
+            let p = document.createElement('p');
+            p.textContent = this.historicoCalculos[i];
+            this.historico.appendChild(p);
+        }
     }
 
-    attNumero(){
+    attNumero(valor){
+        switch (valor){
+            case '<':
+                if( this.valorPrincipal.length > 1){
+                    this.valorPrincipal = this.valorPrincipal.slice(0, -1);
+                }else{
+                    this.valorPrincipal = '0';
+                }
+                                break;
+            case 'C':
+                this.valorPrincipal = '0';
+                this.valorSecundario = '0';
+                this.operacao = '+';
+                this.calculando = false;
+                break;
+            case '.':
+                if( !this.valorPrincipal.includes('.') ){
+                    this.valorPrincipal += valor;
+                }
+                break;
+            default:
+                if ( this.valorPrincipal === '0'){
+                    this.valorPrincipal = valor;
+                } else {
+                    this.valorPrincipal += valor;
+                }
+                break;
+        }
+        this.attValores();
+    }
 
+    attOperacao(op){
+        this.operacao = op;
+        if( this.calculando ){
+            this.calcular();
+            this.valorSecundario = this.resultadoCalculo;
+        }else{
+            this.valorSecundario = this.valorPrincipal;
+        }
+        this.valorPrincipal = '0';
+        this.attValores();  
+        this.calculando = true;
+    }
+
+    attCalcular(){
+        this.calculando = false;
+        this.calcular();
+        this.valorPrincipal = this.resultadoCalculo;
+        this.valorSecundario = '0';
+        this.attValores();
+    }
+
+    calcular(){
+        switch ( this.operacao ){
+            case '+':
+                this.resultadoCalculo = parseFloat(this.valorSecundario) + parseFloat(this.valorPrincipal);             
+                break;
+            case '-':
+                this.resultadoCalculo = parseFloat(this.valorSecundario) - parseFloat(this.valorPrincipal);
+                break;
+            case '*':
+                this.resultadoCalculo = parseFloat(this.valorSecundario) * parseFloat(this.valorPrincipal);
+                break;
+            case '/':
+                if ( this.valorPrincipal != '0'){
+                    this.resultadoCalculo = parseFloat(this.valorSecundario) / parseFloat(this.valorPrincipal);
+                }else{
+                    alert('Divsão por 0 não existe!!!')
+                }                
+                break;
+            case '%':
+                this.resultadoCalculo = this.valorPrincipal / 100;
+                this.operacao = '*'; 
+                break;
+            default:
+                this.valorPrincipal = '0';
+                this.valorSecundario = '0';
+                this.operacao = '+';
+                break;
+        }
+        this.historicoCalculos.push(this.valorSecundario + '  ' + this.operacao + '  ' + this.valorPrincipal + '  =  ' + this.resultadoCalculo);
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
+import { Calculadora } from './calculadora.js';
 import { MenuController } from './controller_header.js';
 
 document.addEventListener("DOMContentLoaded", () => {
     const menuController = new MenuController();
+    const calculadora = new Calculadora();
+
+    window.attNumero = (valor) => calculadora.attNumero(valor);
+    window.attOperacao = (op) => calculadora.attOperacao(op);
+    window.attCalcular = ()=> calculadora.attCalcular();
     window.controleMenu = () => menuController.attMenu();
 });
